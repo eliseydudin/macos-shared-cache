309c6fe54d444ca8c704cf8b3b313afda8a38972
eliseydudin <dudinelisey630@gmail.com>
2024-10-12 16:18:47 +0300

    better docs

--- examples/readme-swift.swift
+++ examples/readme-swift.swift
@@ -1,7 +1,14 @@
 ///
 ///  Example from `objc-runtime.md` but in Swift
 ///  Used to check how Swift classes are stored in `.dylib`s
-///
+/// 
+///  After checking via `nm` it appears that Swift classes are stored 
+///  pretty much the same as in Objective C, the only difference is that
+///  Swift classes are mangled to prevent duplicate symbols.
+/// 
+///  Example (ObjC -> Swift):
+///  `_OBJC_CLASS_$_ExampleClass` -> `_OBJC_CLASS_$__TtC4main12ExampleClass`
+/// 
 
 import Foundation
 
